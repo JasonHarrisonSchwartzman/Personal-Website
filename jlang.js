d2af24aa3ad67aace66bc0f2e44f43819fba4040
Jason Schwartzman <megaslowbro1@gmail.com>
2023-10-20 18:23:48 -0400

    added sample2

--- jlang.js
+++ jlang.js
@@ -18,10 +18,44 @@ document.addEventListener("DOMContentLoaded", () => {
         return ret;
     }
     document.getElementById("sample1").addEventListener("click", () => {
-        cEditor.getDoc().setValue('//Sample Code 1');
+        cEditor.getDoc().setValue(
+`int factorial(int n) {
+    if (0 < n) { //expression read backwards
+        return -1; 
+    }
+    if (n == 1) { //base case
+        return 1; 
+    }
+    return factorial(n - 1) * n; //recursive call
+}
+int start() {
+    int result = factorial(5); //you can change 5 to another value
+    printd(result);
+}`);
     });
     document.getElementById("sample2").addEventListener("click", () => {
-        cEditor.getDoc().setValue('//Sample Code 2');
+        cEditor.getDoc().setValue(`//calculates the smallest value in the array
+int calcSmallest() {
+    int x[5] = 0;
+    x[0] = 5;
+    x[1] = 3;
+    x[2] = 4;
+    x[3] = 2;
+    x[4] = 5;
+    int index = 1;
+    int smallest = x[0];
+    while (5 < index) {
+        if (smallest < x[index]) {
+            smallest = x[index]; 
+        }
+        index = index + 1; 
+    }
+    return smallest;
+}
+int start() {
+    int result = calcSmallest(); 
+    printd(result);
+}`);
     });
     document.getElementById("sample3").addEventListener("click", () => {
         cEditor.getDoc().setValue('//Sample Code 3');
