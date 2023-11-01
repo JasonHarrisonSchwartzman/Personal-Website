a17121d9a8cf35f6f68ace63e71c5463654490bc
Jason Schwartzman <megaslowbro1@gmail.com>
2023-11-01 03:01:15 -0400

    changed order of printing database

--- timestamps.js
+++ timestamps.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
     .then(data => {
         console.log(data.result);
         timestamps = "";
-        for (let i = 0; i < data.result.length; i++) {
+        for (let i = data.result.length - 1; i > -1; i--) {
             timestamps+=data.result[i]+'\n';
         }
         document.getElementById("timestamps").textContent = `${timestamps}`;
