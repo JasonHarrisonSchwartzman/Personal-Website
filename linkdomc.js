b26b41b5b0946c56eb1503ad46dd6668a6c0a152
System Administrator <megaslowbro1@gmail.com>
2024-11-01 13:43:34 -0400

    added stuff

--- linkdomc.js
+++ linkdomc.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
     })
     .then(response => response.json())
     .then(data => {
-        console.log(data);
+        console.log(data.result);
         document.getElementById("timestamps").textContent = `${data.result.split("\n")[0]}`;
     })
     .catch(error => {
