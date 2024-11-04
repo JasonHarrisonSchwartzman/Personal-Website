18136eca5d23154f86a6ae26fd34c8cf003073ba
System Administrator <megaslowbro1@gmail.com>
2024-11-03 19:12:05 -0500

    added stuff

--- linkdomc.js
+++ linkdomc.js
@@ -9,8 +9,21 @@ document.addEventListener("DOMContentLoaded", () => {
     .then(data => {
         console.log(data.result);
         let number = data.result.split("\n")[0];
+        const date1 = new Date(data.result.split("\n")[1]);
+        const date2 = new Date(data.result.split("\n")[2]);
+
+        const diff = date2 - date1;
+        const days = diff / (1000 * 60 * 60 * 24);
+        let string = "It has been ";
+        if (days == 1) {
+            string+= "1 day since linkdomc has played Yasuo.";
+        }
+        else {
+            string+= days + " days since linkdomc has played Yasuo.";
+        }
         let formattedNumber = Number(number).toLocaleString();
         document.getElementById("timestamps").textContent = `${"Yasuo Mastery Points: " + formattedNumber}`;
+        document.getElementById("days").textContent = `${string}`;
     })
     .catch(error => {
         console.log("error");
